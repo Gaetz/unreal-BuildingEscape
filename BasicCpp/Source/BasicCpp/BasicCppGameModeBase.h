ddbd91d0c48c20bf9d786e41e606df23f861de0d
Gaetan Blaise-Cazalet <gblaisec@artfx.fr>
2019-08-27 15:57:57 +0200

    Test cpp project

---
+++ BasicCpp/Source/BasicCpp/BasicCppGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BasicCppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASICCPP_API ABasicCppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
