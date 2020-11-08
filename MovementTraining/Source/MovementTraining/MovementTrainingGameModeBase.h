338c2b168ecfa51b00394159d8817c4228746915
Gaetz <gaetz@rufflerim.com>
2020-11-08 07:13:15 +0100

    Movement training start

---
+++ MovementTraining/Source/MovementTraining/MovementTrainingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MovementTrainingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOVEMENTTRAINING_API AMovementTrainingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
